8c6b9447e7072ebd0c271e36844e5c5ba9618d9d
Mihai Roman <mihairoman.r@gmail.com>
2019-02-07 23:00:00 +0200

    Added apollo and typescript libraries.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,9 @@ import Layout from '../components/Layout'
 const IndexPage: React.FunctionComponent = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1>Hi from Next.js 👋</h1>
       <p><Link href='/about'><a>About</a></Link></p>
+      <button>Call login mutation</button>
     </Layout>
   )
 }
